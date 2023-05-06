6681b575643a8a0562e18aaef794620051c54f24
Daniel Sehaik <daniel.sehaik@solaredge.com>
2023-05-06 12:43:29 +0300

    refactor

--- app/app.py
+++ app/app.py
@@ -2,12 +2,9 @@ from enum import Enum
 
 from flask import Flask, request, jsonify
 import boto3
-import datetime
+from datetime import datetime
 
 app = Flask(__name__)
-
-# dictionary to store parking lot data, key = ticket id, value = [license plate, parking lot id, entry time]
-parking_lot_data = {}
 
 TABLE_NAME = 'ParkingLotDB'
 dynamodb_client = boto3.client('dynamodb')
@@ -22,12 +19,10 @@ class DynamoDbKeys(Enum):
 
 @app.route('/entry', methods=['POST'])
 def entry_parking():
-    # POST /entry?plate=123-123-123&amp;parkingLot=382
-    entry_time = datetime.datetime.now()
+    entry_time = datetime.now()
     plate_number = request.args.get('plateNumber')
     parking_lot = request.args.get('parkingLot')
 
-    # generate ticket id by concatenating plate and current timestamp
     ticket_id = calculate_unique_ticket_id(entry_time, plate_number)
 
     dynamodb_client.put_item(TableName=TABLE_NAME,
@@ -39,34 +34,32 @@ def entry_parking():
     return jsonify({'ticketId': ticket_id})
 
 
-def calculate_unique_ticket_id(datetiem, plate_number):
-    return str(plate_number) + '.' + datetiem.strftime('%s')
+def calculate_unique_ticket_id(datetime, plate_number):
+    return str(plate_number) + '.' + datetime.strftime('%s')
 
 
 @app.route('/exit', methods=['POST'])
 def exit_parking():
-    # POST /exit?ticketId=1234
     ticket_id = request.args.get('ticketId')
-
-    # retrieve entry data from dictionary
-    entry_data = parking_lot_data.get(ticket_id)
 
     car_data = dynamodb_client.get_item(TableName=TABLE_NAME,
                                         Key={DynamoDbKeys.TICKET_ID.value: {'S': ticket_id}})
     if car_data is None:
         return jsonify({'error': 'Invalid ticketId'})
-    entry_time = datetime.datetime.fromtimestamp(int(car_data["Item"][DynamoDbKeys.ENTRY_TIMESTAMP.value]['S']))
+    entry_time = datetime.fromtimestamp(int(car_data["Item"][DynamoDbKeys.ENTRY_TIMESTAMP.value]['S']))
     plate_number = car_data["Item"][DynamoDbKeys.PLATE_NUMBER.value]['S']
     parking_lot = car_data["Item"][DynamoDbKeys.PARKING_LOT.value]['S']
 
     # calculate parked time in minutes
-    parked_time = int((datetime.datetime.now() - entry_time).total_seconds() / 60)
+    parked_time_minutes = int((datetime.now() - entry_time).total_seconds() / 60)
 
     # calculate charge based on parked time (rounded up to nearest 15 minutes)
-    charge = round(parked_time / 15) * 10
+    charge = round(parked_time_minutes / 15) * 2.5
 
-    return jsonify(
-        {'licensePlate': plate_number, 'parkedTime': parked_time, 'parkingLot': parking_lot, 'charge': charge})
+    return jsonify({'licensePlate': plate_number,
+                    'parkedTime': parked_time_minutes,
+                    'parkingLot': parking_lot,
+                    'charge': charge})
 
 
 if __name__ == '__main__':
